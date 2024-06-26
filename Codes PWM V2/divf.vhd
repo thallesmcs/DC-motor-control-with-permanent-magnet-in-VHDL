069087e66db5e33ed970aa1f89bfbdb3ca465f42
Rafael B. Schvittz <57268243+schvittz@users.noreply.github.com>
2024-06-26 14:06:23 -0300

    Update divf.vhd

--- Codes PWM V2/divf.vhd
+++ Codes PWM V2/divf.vhd
@@ -5,19 +5,24 @@ entity divf is
     port(
         clk                       : in std_logic;
         rst                       : in std_logic;
-        s_SEL_PR                    : in  std_logic_vector (1 downto 0);
-        Q1, Q2, Q3, Q4, OUT_CLK     : out std_logic
+        s_SEL_PR                  : in  std_logic_vector (1 downto 0);
+        OUT_CLK                   : out std_logic
     );
 end divf;
 
 architecture dividendo of divf is
+    signal jk_input : std_logic := '1';
+    
     signal clk2    : std_logic; 
     signal clk4    : std_logic;
     signal clk8    : std_logic;
+    signal clk16    : std_logic;
+
 
     component JK_FF
         port(
-            clk: in std_logic;
+            J, K : in std_logic;
+            s_clk: in std_logic;
             s_rst: in std_logic;
             Q: out std_logic
         );
@@ -27,35 +32,39 @@ begin
 
     df1: JK_FF
         port map(
-            clk => clk,
+            J =>jk_input,
+            k =>jk_input,
+            s_clk => clk,
             s_rst => rst,
             Q => clk2             -- CLK divido por 2
         );
 
     df2: JK_FF
         port map(
-            clk => clk2,
+            J =>jk_input,
+            k =>jk_input,
+            s_clk => clk2,
             s_rst => rst,
             Q => clk4             -- CLK divido por 4
         );
 
     df3: JK_FF
         port map(
-            clk => clk4,
+            J =>jk_input,
+            k =>jk_input,
+            s_clk => clk4,
             s_rst => rst,
             Q => clk8             -- CLK divido por 8
         );
     
     df4: JK_FF
         port map(
-            clk => clk8,
+            J =>jk_input,
+            k =>jk_input,
+            s_clk => clk8,
             s_rst => s_rst,
-            Q => Q4               -- CLK divido por 16
+            Q => clk16               -- CLK divido por 16
         );
-
-    Q1 <= clk2;
-    Q2 <= clk4;
-    Q3 <= clk8;
 
     ---------- Lembrando que a frequencia é divida 2^n, onde n é o numero de FF 
     ---------- F_clk = CLk / 2^n
@@ -65,10 +74,10 @@ begin
 
                 case s_SEL_PR is
 
-                    when "00" => OUT_CLK <= Q4      -- CLK/16
-                    when "10" => OUT_CLK <= Q2      -- CLK/4
-                    when "01" => OUT_CLK <= clk   -- CLK
-                    when others => OUT_CLK <= '0' 
+                    when "00" => OUT_CLK <= clk16      -- CLK/16
+                    when "10" => OUT_CLK <= clk8      -- CLK/8
+                    when "01" => OUT_CLK <= clk4       -- CLK/4
+                    when others => OUT_CLK <= clk2   -- CLK/2
                 
                     end case;
 
