e49f06ffc4a1a6200a477907b90984cf957d9c21
Thalles M. <106935500+thallesmcs@users.noreply.github.com>
2024-07-22 16:56:00 -0300

    Update PWM.vhd Final Version

--- Codes PWM V2/PWM.vhd
+++ Codes PWM V2/PWM.vhd
@@ -12,13 +12,6 @@ entity PWM is
     );
 end entity;
 architecture RTL of PWM is
-	
-	-- Fclk
-	-- Fpwm = ------------------------
-	-- (TIMER + 1)*PRESCALER
-	--
-	-- TIMER 8bits -> 0 to 255
-	-- PRESCALER   -> 1, 4, or 16
 
 	signal s_OUT_CLK   : std_logic; 
 	signal CYCLE_OFF   : std_logic_vector (7 downto 0);
@@ -27,7 +20,6 @@ architecture RTL of PWM is
 	signal TIMER       : std_logic_vector (7 downto 0);
 	signal DUTY        : std_logic_vector (7 downto 0) := "00000000";
 	signal SW_s        : std_logic_vector (7 downto 0);
---	signal cont_duty  : std_logic_vector (7 downto 0) := "00000000";
 
 	component divf
 		port (
@@ -51,23 +43,10 @@ architecture RTL of PWM is
 		SW_s <= SW;  -- Lembrar de religar os SW para placa
 		DUTY <= SW_s;
 
------Process para contar o duty para ver na pratica aumentando o Ciclo ON-----		
---	process(s_PWM_OUT)
---	begin
---		cont_duty <= cont_duty + 1;               
---			if (cont_duty = "00100000") then
---				cont_duty <= "00000000";
---			end if;
---	end process;
-
---	DUTY <= cont_duty;
-------------------------------------------------------------------------------
-
 	TIMER <= "01000000";          --Maximo de Pulsos para o Contador e Tamanho maximo da PWM de 0-1
 	
 	CYCLE_OFF <= TIMER - DUTY - '1';  -- Tamanho do Ciclo off do pulso de PWM
 	
-		
 	process(s_OUT_CLK,RST)
 	begin
 		if( RST = '1' ) then
