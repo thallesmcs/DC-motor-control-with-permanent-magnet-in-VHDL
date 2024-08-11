33fd0a74eddba491a1ea5d4d83106c74683f5fb0
Thalles M. <106935500+thallesmcs@users.noreply.github.com>
2024-08-11 18:05:47 -0300

    Update sensor_hall.vhd

--- Sensor Hall/sensor_hall.vhd
+++ Sensor Hall/sensor_hall.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+--use IEEE.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
 
 entity sensor_hall is
   port (
@@ -12,30 +13,28 @@ end sensor_hall;
 
 architecture rpm of sensor_hall is
 
-    signal cont      :  std_logic_vector(7 downto 0);
+    signal cont      :  std_logic_vector(7 downto 0) := "00000000";
     signal val_pos   :  std_logic_vector(7 downto 0);
 
 begin
 
   process(clk)
   begin
-    if (rising_edge(CLK)) then
+    if (rising_edge(clk)) then
       
-      if (val_hall = 0) then
-          cont <= cont + '1';
+      if (val_hall = '0') then
+          cont <= cont + "00000001";
       
-      else 
-          val_pos <= cont;    
-          cont <= '0';
+      elsif (val_hall = '1') then 
+          if (cont > "00000000") then
+            val_pos <= cont;
+          end if; 
+          cont <= "00000000";
       end if;
         
     end if;
   end process;
-
 rpm <= val_pos;
       
 end rpm;
-
-
-    
 
