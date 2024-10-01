794b8bb856c3ab6cd6eceb649aebc85f12736229
Thalles M. <106935500+thallesmcs@users.noreply.github.com>
2024-10-01 16:18:27 -0300

    Atualizar o Codes PWM V2

---
+++ Codes PWM V2/D_FF.vhd
@@ -0,0 +1,38 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity D_FF is
+    port(
+        D, s_clk: in std_logic;
+        s_rst: in std_logic;
+        Q: out std_logic
+    );
+end D_FF;
+
+architecture ark of D_FF is
+
+    signal qs: std_logic := '0';
+    signal d: std_logic := '0';
+
+begin
+
+    d <= D;
+
+    process(s_clk,s_rst)
+    begin
+
+        if( s_rst = '1' ) then
+            qs <= '0';
+
+        elsif rising_edge(s_clk) then
+            case d is
+                    when "00" =>  qs <= '0';
+                    when "10" =>  qs <= '0';
+                    when "01" =>  qs <= '0';
+                    when "11" =>  qs <= qs; 
+            end case;
+        end if;
+    end process;
+
+    Q <= qs;
+end ark;
