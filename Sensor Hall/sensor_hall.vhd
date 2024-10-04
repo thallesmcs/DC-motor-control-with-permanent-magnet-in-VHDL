4e4261941b3ef43c2f6ae60c75bf9680ba0b1e7f
Thalles M. <106935500+thallesmcs@users.noreply.github.com>
2024-10-04 19:25:42 -0300

    Atualizar o sensor_hall.vhd

--- Sensor Hall/sensor_hall.vhd
+++ Sensor Hall/sensor_hall.vhd
@@ -4,21 +4,41 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 entity sensor_hall is
+
+	generic (
+					R := integer := 16);
+
   port (
-    	clk       	:  	in  std_logic;
+    	clk, rst       	:  	in  std_logic;
     	val_hall  	:  	in  std_logic;
-	clk_50 		: 	out std_logic;
-    	rpm       	:  	out std_logic_vector(7 downto 0));
+		clk_50 		: 	out std_logic;
+    	rpm       	:  	out std_logic_vector(R-1 downto 0));
 	
 end sensor_hall;
 
 architecture rpm of sensor_hall is
 
-    	signal tempo      	:  	std_logic_vector(7 downto 0) := "00000000";
-    	signal val_pos   	:  	std_logic_vector(7 downto 0);
-	signal count_f		:  	std_logic_vector (15 downto 0) := "0000000000000000" ; --integer range 0 to 49999;
+    	signal tempo      	:  	std_logic_vector(R-1 downto 0) := "0000000000000000";
+    	signal val_pos   	:  	std_logic_vector(R-1 downto 0);
+		signal count_f		:  	std_logic_vector (R-1 downto 0) := "0000000000000000" ; --integer range 0 to 49999;
 	signal clk_ms 		: 	std_logic := '0';
-	constant f_clk		:	std_logic_vector (15 downto 0) := "1100001101001111"; --integer := 49999; -- (110000110100111 =  24999)
+	constant f_clk		:	std_logic_vector (R-1 downto 0) := "1100001101001111"; --integer := 49999; -- (110000110100111 =  24999)
+
+------------ Divisor de bits-----------
+
+	component divisor 
+		port(
+			clk,reset 	: in std_logic;
+			start 			: in std_logic;
+			divisor 		: in std_logic_vector(N-1 downto 0);
+			dividendo 	: in std_logic_vector(N-1 downto 0);
+			terminou 	: out std_logic;
+			quociente	: out std_logic_vector(N-1 downto 0);
+			resto		: out std_logic_vector(N-1 downto 0));
+
+	end component;
+
+---------------------------------------
 
 begin
 
@@ -27,7 +47,11 @@ clk_50<= clk_ms;
 process(clk)
 begin
 	
-	if (rising_edge(clk)) then
+	if(rst ='1') then
+		count_f <= "0000000000000000";
+		clk_ms <= '0';
+	
+	elsif (rising_edge(clk)) then
 		count_f <= count_f + "0000000000000001";
 	
 		if (count_f = f_clk) then
@@ -40,7 +64,13 @@ end process;
 
 process(clk_ms)
 begin
-    if (rising_edge(clk_ms) or falling_edge(clk_ms)) then 
+
+	if(rst ='1') then
+		tempo <= "0000000000000000";
+		val_pos <= '0';
+		rpm = "0000000000000000";
+	
+    elsif (rising_edge(clk_ms) or falling_edge(clk_ms)) then 
         if (val_hall = '0') then
             tempo <= tempo + "00000001";
         elsif (val_hall = '1') then 
@@ -70,7 +100,19 @@ end process;
 	     -- Lembrando que o valor do "val_pos" é o valor em binario do tempo levado em milissegundos de uma revolução,
              -- o valor da posição referente ao Hall em relação com o tempo desta posição inicial ate a final.
 
+-----------------------------------------------------------
 
-rpm <= val_pos; --Arrumar com modulo de binary divider
+	divisor_1: divisor 
+			port map(
+				clk => clk;
+				reset => rst;
+				start => start;
+				divisor  => val_pos;
+				dividendo => f_clk;
+				terminou 	=> terminou;
+				quociente	=> quociente;
+				resto => resto);			
+
+rpm <= quociente; --Arrumar com modulo de binary divider
 
 end rpm;
